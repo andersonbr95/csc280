259c1c0b6755f371d603d25feff641aad3c4d8b3
Bryan Anderson <andersonbr95@gmail.com>
2024-01-14 15:25:47 -0600

    initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import module1.HelloWorld;
+
+public class App {
+
+    static HelloWorld helloWorld = new HelloWorld();
+
+    public static void main(String[] args) throws Exception {
+        helloWorld.helloWorld("Bryan");
+    }
+
+
+}
---
+++ src/module1/HelloWorld.java
@@ -0,0 +1,9 @@
+package module1;
+
+public class HelloWorld {
+    
+    public void helloWorld(String name){
+       System.out.println("Hello World, my name is " + name);
+    }
+
+}
