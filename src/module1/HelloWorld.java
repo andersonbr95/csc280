f4e61ab6a875d79fb0c765f257eb816e9e68d438
Wiegel <lckywgl81@outlook.com>
2024-01-23 19:46:04 -0600

    Module 1 | Updated Central App File

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -2,8 +2,6 @@ package module1;
 
 public class HelloWorld {
     
-    public void helloWorld(String name){
-       System.out.println("Hello World, my name is " + name);
-    }
+    public void helloWorld(String name){System.out.println("Hello World, my name is " + name);}
 
 }
