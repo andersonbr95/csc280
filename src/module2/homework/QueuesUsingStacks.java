eb0a1c6b60f79996320688615430d9ef7e088d6c
Bryan Anderson <andersonbr95@gmail.com>
2024-01-30 20:16:34 -0600

    Module 2 | Homework & Source Code

---
+++ src/module2/homework/QueuesUsingStacks.java
@@ -0,0 +1,32 @@
+package module2.homework;
+
+import module2.stack.GenericStack;
+
+public class QueuesUsingStacks {
+
+    private GenericStack<Integer> stack1;
+    private GenericStack<Integer> stack2;
+
+    public QueuesUsingStacks(){
+        stack1 = new GenericStack<Integer>(1);
+        stack2 = new GenericStack<Integer>(2);
+    }
+
+    //TODO write this method using the 2 stacks provided above only
+    public void enqueue(Integer value){
+
+    }
+
+    //TODO Write this method using the 2 stacks provided above only
+    public Integer dequeue(){
+        return null;
+    }
+
+    public int peek(){
+        return stack1.peek();
+    }
+
+    public boolean isEmpty(){
+        return stack1.isEmpty();
+    }
+}
