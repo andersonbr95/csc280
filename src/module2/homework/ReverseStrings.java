0cd62863da10114da996f07c8a4f9c44bfd99725
Fattoushy <imnotfattash@gmail.com>
2024-02-09 11:01:38 -0600

    Module 2 | Comment

--- src/module2/homework/ReverseStrings.java
+++ src/module2/homework/ReverseStrings.java
@@ -1,16 +1,37 @@
 package module2.homework;
 
+import java.util.*;
+
 public class ReverseStrings {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String myString = "hello";
         String reversedString = reverseString(myString);
 
         System.out.println(reversedString);
         //Expected Output 'olleh'
+
+
+        /*TODO HOMEWORK CREATE THIS METHOD USING STACKS*/
+
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : myString.toCharArray()) {
+            stack.push(c);
+        }
+
+        String rvs = "";
+
+        while (!stack.isEmpty()) {
+            rvs += stack.pop();
+        }
+
+        System.out.println(rvs);
     }
 
-    /*TODO HOMEWORK CREATE THIS METHOD USING STACKS*/
+
+
+
     private static String reverseString(String myString) {
         return "";
     }
