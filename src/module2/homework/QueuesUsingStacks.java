a06e672de3264711021047c2108c43089f60375d
SzymonStraczek <sstraczek10@gmail.com>
2024-02-11 17:19:00 -0600

    Completed assignments

--- src/module2/homework/QueuesUsingStacks.java
+++ src/module2/homework/QueuesUsingStacks.java
@@ -14,12 +14,23 @@ public class QueuesUsingStacks {
 
     //TODO write this method using the 2 stacks provided above only
     public void enqueue(Integer value){
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
 
+        stack1.push(value);
+
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
     }
 
     //TODO Write this method using the 2 stacks provided above only
     public Integer dequeue(){
-        return null;
+        if (stack1.isEmpty()) {
+            return null;
+        }
+        return stack1.pop();
     }
 
     public int peek(){
--- src/module2/homework/ReverseStrings.java
+++ src/module2/homework/ReverseStrings.java
@@ -1,5 +1,7 @@
 package module2.homework;
 
+import java.util.Stack;
+
 public class ReverseStrings {
 
     public static void main(String[] args){
@@ -12,7 +14,17 @@ public class ReverseStrings {
 
     /*TODO HOMEWORK CREATE THIS METHOD USING STACKS*/
     private static String reverseString(String myString) {
-        return "";
+        Stack<Character> stack = new Stack<>();
+        char[] characterArray = myString.toCharArray();
+        for(int i = 0; i < characterArray.length; ++i)    {
+            char stringChar = characterArray[i];
+            stack.push(stringChar);
+        }
+        String revString = "";
+        while(!stack.isEmpty()) {
+            revString += stack.pop();
+        }
+        return revString;
     }
 
 }
