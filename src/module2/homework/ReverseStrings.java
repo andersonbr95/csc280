acfe85a41824ecc768a430ddd8de39700eb22d59
Temitayo Adejare <temitayoadejare@icloud.com>
2024-02-11 19:21:48 -0600

    module1 commit

--- src/module2/homework/ReverseStrings.java
+++ src/module2/homework/ReverseStrings.java
@@ -1,18 +1,36 @@
 package module2.homework;
-
+import java.util.Stack;
 public class ReverseStrings {
-
     public static void main(String[] args){
         String myString = "hello";
         String reversedString = reverseString(myString);
-
         System.out.println(reversedString);
-        //Expected Output 'olleh'
+//Expected Output 'olleh'
     }
-
     /*TODO HOMEWORK CREATE THIS METHOD USING STACKS*/
     private static String reverseString(String myString) {
-        return "";
+
+        // The Method .toCharArray() will help you loop through the String passed
+        //into the argument
+        char[] charArray = myString.toCharArray();
+
+        //You’ll want to create a Generic Stack of type ‘Character’.
+        Stack<Character> stack = new Stack<>();
+
+        for(char a : charArray) {
+            stack.push(a);
+
+        }
+        StringBuilder reversed = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            reversed.append(stack.pop());
+
+
+        }
+        //return the reversed string.
+        return reversed.toString();
     }
+
 
 }
